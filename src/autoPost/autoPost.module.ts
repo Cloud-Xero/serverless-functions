3bf57941295b9500776b360810aede7e32080384
Cloud-Xero <hrk.komamiya@gmail.com>
2024-02-05 22:49:29 +0900

    feat: export AutoPostModule

--- src/autoPost/autoPost.module.ts
+++ src/autoPost/autoPost.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AutoPostController } from './autoPost.controller';
 import { AutoPostService } from './autoPost.service';
+import { NotionModule } from 'src/services/notion/notion.module';
+import { InstagramModule } from 'src/services/instagram/instagram.module';
 
 @Module({
-  imports: [],
+  imports: [NotionModule, InstagramModule],
   controllers: [AutoPostController],
   providers: [AutoPostService],
 })
-export class HelloWorldModule {}
+export class AutoPostModule {}
